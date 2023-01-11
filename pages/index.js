d8c39744041a46f4c33e6161c94bc1cf70641a5f
CaioIbraim <caiofabiocosta@gmail.com>
2023-01-10 21:56:02 -0300

    Carregamento da página

--- pages/index.js
+++ pages/index.js
@@ -5,45 +5,60 @@ import Slider from '../components/Slider';
 import { SliderData } from '../components/SliderData';
 import { supabase } from '../supabase'
 import { useEffect, useState } from 'react'
-
-
+import { Oval } from  'react-loader-spinner'
 
 export default function Home() {
 
-
   const [loading, setLoading] = useState(true)
   const [produtos, setProdutos] = useState([])
- 
+  const [statusPage, setStatusPage] = useState(false)
+  const [produtosLoaded, setProdutosLoaded] = useState(false)
   
   useEffect(() => {
-      getProfile()
+      getProdutos()
   }, [loading])
 
 
-
-  const getProfile = async () => {
-    try {
-      setLoading(true)
-    
-      let { data, error, status } = await supabase
-        .from('produto')
-        .select(`*`)
-       
-      if (error && status !== 406) {
-        throw error
-      }
-
-      setProdutos(data)
-      
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setLoading(false)
+  const vrfStatusPage = () => {
+    if(produtosLoaded == true){
+      setStatusPage(true)
     }
   }
 
+  const handleFiltrarProduto = (value) => {
+    
+    if(value == ""){
+      getProdutos()
+      return false
+    }else if(value.length > 3){      
+          const regexp = new RegExp(value, 'i');      
+          let find = produtos.filter(x => x.title.toLowerCase().includes(value.toLowerCase()))
+          if (find.length == 0) {
+              getProdutos()
+              return false
+          }
+          setProdutos(find)
+    }
+  }
 
-
+  const getProdutos = async () => {
+    try {
+      setStatusPage(false)    
+      let { data, error, status } = await supabase
+        .from('produto')
+        .select(`*`)       
+      if (error && status !== 406) {
+        throw error
+      }
+      setProdutos(data) 
+      setProdutosLoaded(true)
+          
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      vrfStatusPage() 
+    }
+  }
   
   return (
     <div>
@@ -66,19 +81,54 @@ export default function Home() {
       </Head>
 
       <Navbar />
-      <Slider slides={SliderData} />
 
-    <div className="flex">
-      <section className='w-10/12  mx-auto md:p-10'>
-        <div className="pt-3 pb-5">
-          <span className="text-slate-400">#Pacotes e Promoções</span>
-          <h1 className='text-1xl md:text-4xl mt-2 text-left uppercase  text-slate-800'>Nossos Produtos</h1>
-        </div>       
-        <div className='mt-2 grid grid-cols-2 md:grid-cols-4 gap-4'>
-          {produtos.map(product => <Product product={product} />)}
-        </div>
-      </section>
-    </div>
+{statusPage == true ?
+        <>
+            <Slider slides={SliderData} />
+            <div className="flex">
+              <section className='w-10/12  mx-auto md:p-10'>
+                
+                <div className="pt-3 pb-5">
+                  <span className="text-slate-400">#Pacotes e Promoções</span>
+                  <h1 className='text-1xl md:text-4xl mt-2 text-left uppercase  text-slate-800'>Nossos Produtos</h1>
+                </div> 
+
+                <div className="pt-3 pb-5">
+                  <span className="text-slate-400">#Pesquisar</span>
+                  <input type="text" placeholder="Pesquise seu produto aqui" onChange={(e) => {handleFiltrarProduto(e.target.value)} } className="w-full mt-2 uppercase text-slate-600 border-slate-400 border-2 p-1 rounded-md " />
+                </div> 
+
+                <div className='mt-2 grid grid-cols-2 md:grid-cols-4 gap-4'>
+                  {produtos.map(product => <Product product={product} />)}
+                </div>
+              </section>
+            </div>
+        </>
+
+: 
+<>
+
+  <div className="flex justify-center h-screen items-center">
+    <Oval
+      height={80}
+      width={80}
+      color="red"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel='oval-loading'
+      secondaryColor="#f00"
+      strokeWidth={2}
+      strokeWidthSecondary={2}
+    />
+  </div>
+
+</>
+
+
+}
+
+
     </div>
   )
 }
